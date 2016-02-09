fce374555bab8bc3ae14268f51f4c367874bb834
ineumann <idriss.neumann@docapost.fr>
2016-02-09 08:36:00 +0100

    English javadoc

--- src/main/java/com/bblvertx/route/AbstractRoute.java
+++ src/main/java/com/bblvertx/route/AbstractRoute.java
@@ -1,36 +1,38 @@
 package com.bblvertx.route;
 
 import static com.bblvertx.SeConstants.PREFIX_URL;
-import io.vertx.ext.web.Router;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.bblvertx.utils.singleton.RouteContext;
 
+import io.vertx.ext.web.Router;
+
 /**
- * Abstract pour les routes (routines).
+ * Abstract class for all routes (job).
  * 
  * @author Idriss Neumann <neumann.idriss@gmail.com>
  *
  */
 public abstract class AbstractRoute implements Route {
-	private static final Logger LOGGER = LogManager.getLogger(AbstractRoute.class);
+  private static final Logger LOGGER = LogManager.getLogger(AbstractRoute.class);
 
-	protected RouteContext ctx;
+  protected RouteContext ctx;
 
-	/**
-	 * Constructeur permettant d'initaliser une route.
-	 * 
-	 * @param url
-	 * @param contentType
-	 * @param router
-	 */
-	public AbstractRoute(String url, String contentType, Router router, RouteContext ctx) {
-		this.ctx = ctx;
-		router.get(String.format(PREFIX_URL, url)).handler(req -> {
-			LOGGER.info("Lauching route " + url);
-			req.response().putHeader("content-type", contentType).end(this.proceed(req.request(), req.response()));
-		});
-	}
+  /**
+   * Initializing route in the router of verticle.
+   * 
+   * @param url
+   * @param contentType
+   * @param router
+   */
+  public AbstractRoute(String url, String contentType, Router router, RouteContext ctx) {
+    this.ctx = ctx;
+    router.get(String.format(PREFIX_URL, url)).handler(req -> {
+      LOGGER.info("Lauching route " + url);
+      req.response().putHeader("content-type", contentType)
+          .end(this.proceed(req.request(), req.response()));
+    });
+  }
 }
--- src/main/java/com/bblvertx/route/AbstractSearchSingleFieldRoute.java
+++ src/main/java/com/bblvertx/route/AbstractSearchSingleFieldRoute.java
@@ -9,9 +9,6 @@ import static com.bblvertx.utils.JSONUtils.objectTojsonQuietly;
 import static org.apache.commons.collections.CollectionUtils.isEmpty;
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static org.elasticsearch.index.query.QueryBuilders.regexpQuery;
-import io.vertx.core.http.HttpServerRequest;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.ext.web.Router;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,102 +22,111 @@ import org.elasticsearch.search.SearchHit;
 import com.bblvertx.pojo.SearchResult;
 import com.bblvertx.utils.singleton.RouteContext;
 
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+
 /**
- * Route abstraite pour rechercher un champs dans un index.
+ * Abstract searching single field route.
  * 
  * @author Idriss Neumann <neumann.idriss@gmail.com>
  *
  */
 public abstract class AbstractSearchSingleFieldRoute extends AbstractSearchIndexRoute {
-	private static final Logger LOGGER = LogManager.getLogger(AbstractSearchSingleFieldRoute.class);
+  private static final Logger LOGGER = LogManager.getLogger(AbstractSearchSingleFieldRoute.class);
 
-	/**
-	 * Constructeur.
-	 * 
-	 * @param url
-	 * @param contentType
-	 * @param router
-	 * @param ctx
-	 */
-	public AbstractSearchSingleFieldRoute(String url, String contentType, Router router, RouteContext ctx) {
-		super(url, contentType, router, ctx);
-	}
+  /**
+   * Constructor.
+   * 
+   * @param url
+   * @param contentType
+   * @param router
+   * @param ctx
+   */
+  public AbstractSearchSingleFieldRoute(String url, String contentType, Router router,
+      RouteContext ctx) {
+    super(url, contentType, router, ctx);
+  }
 
-	/**
-	 * Méthode de recherche d'un champs dans un index de façon paginée.
-	 * 
-	 * @param request
-	 * @param response
-	 * @param indexName
-	 * @param fieldName
-	 * @return String : réponse sérialisée en JSON
-	 */
-	public String proceed(HttpServerRequest request, HttpServerResponse response, String indexName, String fieldName) {
-		// Contrôles des paramètres
-		Integer startIndex = assertParamNumeric(request.getParam("startIndex"), String.format(MSG_BAD_REQUEST_MUST_BE_NUMERIC, "startIndex"));
-		Integer maxResults = assertParamNumeric(request.getParam("maxResults"), String.format(MSG_BAD_REQUEST_MUST_BE_NUMERIC, "maxResults"));
+  /**
+   * Search with pagination.
+   * 
+   * @param request
+   * @param response
+   * @param indexName
+   * @param fieldName
+   * @return String : réponse sérialisée en JSON
+   */
+  public String proceed(HttpServerRequest request, HttpServerResponse response, String indexName,
+      String fieldName) {
+    // Contrôles des paramètres
+    Integer startIndex = assertParamNumeric(request.getParam("startIndex"),
+        String.format(MSG_BAD_REQUEST_MUST_BE_NUMERIC, "startIndex"));
+    Integer maxResults = assertParamNumeric(request.getParam("maxResults"),
+        String.format(MSG_BAD_REQUEST_MUST_BE_NUMERIC, "maxResults"));
 
-		assertParamNotEmpty(startIndex, String.format(MSG_BAD_REQUEST, "startIndex"));
-		assertParamNotEmpty(maxResults, String.format(MSG_BAD_REQUEST, "maxResults"));
+    assertParamNotEmpty(startIndex, String.format(MSG_BAD_REQUEST, "startIndex"));
+    assertParamNotEmpty(maxResults, String.format(MSG_BAD_REQUEST, "maxResults"));
 
-		List<String> searchCriteres = request.params().getAll("searchCriteres");
+    List<String> searchCriteres = request.params().getAll("searchCriteres");
 
-		BoolQueryBuilder qb = boolQuery();
-		qb.minimumNumberShouldMatch(1);
+    BoolQueryBuilder qb = boolQuery();
+    qb.minimumNumberShouldMatch(1);
 
-		SearchResult<String> result = initSearchResult(Long.valueOf(startIndex), Long.valueOf(maxResults));
-		if (isEmpty(searchCriteres)) {
-			return objectTojsonQuietly(result, SearchResult.class);
-		}
+    SearchResult<String> result =
+        initSearchResult(Long.valueOf(startIndex), Long.valueOf(maxResults));
+    if (isEmpty(searchCriteres)) {
+      return objectTojsonQuietly(result, SearchResult.class);
+    }
 
-		for (String c : searchCriteres) {
-			qb.should(regexpQuery(fieldName, c.toLowerCase() + ".*"));
-		}
+    for (String c : searchCriteres) {
+      qb.should(regexpQuery(fieldName, c.toLowerCase() + ".*"));
+    }
 
-		SearchResponse r = null;
-		try {
-			r = ctx.getEsClient() //
-			        .getClient() //
-			        .prepareSearch(indexName) //
-			        .setQuery(qb) //
-			        .addFields(fieldName) //
-			        .setFrom(startIndex * maxResults) //
-			        .setSize(maxResults).execute() //
-			        .actionGet();
-		} catch (Exception e) {
-			LOGGER.warn(e);
-			return objectTojsonQuietly(result, SearchResult.class);
-		}
+    SearchResponse r = null;
+    try {
+      r = ctx.getEsClient() //
+          .getClient() //
+          .prepareSearch(indexName) //
+          .setQuery(qb) //
+          .addFields(fieldName) //
+          .setFrom(startIndex * maxResults) //
+          .setSize(maxResults).execute() //
+          .actionGet();
+    } catch (Exception e) {
+      LOGGER.warn(e);
+      return objectTojsonQuietly(result, SearchResult.class);
+    }
 
-		result.setTotalResults(r.getHits().getTotalHits());
+    result.setTotalResults(r.getHits().getTotalHits());
 
-		List<String> lstResult = new ArrayList<String>();
+    List<String> lstResult = new ArrayList<String>();
 
-		if (r.getHits().getHits().length > 0) {
-			for (SearchHit hit : r.getHits().getHits()) {
-				List<Object> values = hit.getFields().get(fieldName).getValues();
-				if (isEmpty(values)) {
-					continue;
-				}
+    if (r.getHits().getHits().length > 0) {
+      for (SearchHit hit : r.getHits().getHits()) {
+        List<Object> values = hit.getFields().get(fieldName).getValues();
+        if (isEmpty(values)) {
+          continue;
+        }
 
-				for (Object v : values) {
-					if (null == v) {
-						continue;
-					}
+        for (Object v : values) {
+          if (null == v) {
+            continue;
+          }
 
-					String value = String.valueOf(v).toLowerCase();
-					for (String c : searchCriteres) {
-						if (value.contains(c.toLowerCase()) && !lstResult.contains(value)) {
-							lstResult.add(value);
-							break;
-						}
-					}
-				}
-			}
-		}
+          String value = String.valueOf(v).toLowerCase();
+          for (String c : searchCriteres) {
+            if (value.contains(c.toLowerCase()) && !lstResult.contains(value)) {
+              lstResult.add(value);
+              break;
+            }
+          }
+        }
+      }
+    }
 
-		result.setResults(lstResult);
+    result.setResults(lstResult);
 
-		return objectTojsonQuietly(result, SearchResult.class);
-	}
+    return objectTojsonQuietly(result, SearchResult.class);
+  }
 }
