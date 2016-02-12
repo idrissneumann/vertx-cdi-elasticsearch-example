d0de7482b5bfb009155a827416ff00c2c01bef2c
ineumann <idriss.neumann@docapost.fr>
2016-02-12 19:06:40 +0100

    Adding findbugs and checkstyle

--- src/main/resources/db/init/0_bblvertx_drop_db.sql
+++ src/main/resources/db/init/0_bblvertx_drop_db.sql
@@ -2,5 +2,5 @@
 --unix
 SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = 'bblvertx';
 --Windows
---SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'prodit';
+--SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'bblvertx';
 DROP DATABASE bblvertx;
--- src/test/java/com/bblvertx/tests/verticle/SearchEngineServerTest.java
+++ src/test/java/com/bblvertx/tests/verticle/SearchEngineServerTest.java
@@ -6,22 +6,23 @@ import static com.bblvertx.SeConstants.KEY_TPL_ROUTE_URL;
 import static com.bblvertx.SeConstants.RESPONSE_HTML_TEMPLATE;
 import static com.bblvertx.SeConstants.ROUTE_CONFIG_FILE;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.unit.Async;
-import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+import com.bblvertx.SearchEngineServer;
+import com.bblvertx.tests.AbstractTest;
+import com.bblvertx.utils.FileUtils;
+import com.bblvertx.utils.singleton.PropertyReader;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.bblvertx.SearchEngineServer;
-import com.bblvertx.tests.AbstractTest;
-import com.bblvertx.utils.FileUtils;
-import com.bblvertx.utils.singleton.PropertyReader;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
 
 /**
  * Test sur le verticle SearchEngineServer.
@@ -31,75 +32,79 @@ import com.bblvertx.utils.singleton.PropertyReader;
  */
 @RunWith(VertxUnitRunner.class)
 public class SearchEngineServerTest extends AbstractTest {
-	private Vertx vertx;
-	private PropertyReader propReader;
+  private Vertx vertx;
+  private PropertyReader propReader;
 
-	/**
-	 * Pré-conditions : initialisation du verticle avec injection de dépendance
-	 * via HK2 / CDI.
-	 * 
-	 * @param context
-	 */
-	@Before
-	public void before(final TestContext context) {
-		vertx = Vertx.vertx();
-		DeploymentOptions opt = new DeploymentOptions().setConfig(new JsonObject(FileUtils.file2stringQuietly(getDataDir() + "config.json")));
-		vertx.deployVerticle("java-hk2:" + SearchEngineServer.class.getName(), opt, context.asyncAssertSuccess());
+  /**
+   * Pré-conditions : initialisation du verticle avec injection de dépendance via HK2 / CDI.
+   * 
+   * @param context
+   */
+  @Before
+  public void before(final TestContext context) {
+    vertx = Vertx.vertx();
+    DeploymentOptions opt = new DeploymentOptions()
+        .setConfig(new JsonObject(FileUtils.file2stringQuietly(getDataDir() + "config.json")));
+    vertx.deployVerticle("java-hk2:" + SearchEngineServer.class.getName(), opt,
+        context.asyncAssertSuccess());
 
-		propReader = new PropertyReader();
-	}
+    propReader = new PropertyReader();
+  }
 
-	/**
-	 * Posts-conditions
-	 * 
-	 * @param context
-	 */
-	@After
-	public void after(final TestContext context) {
-		vertx.close(context.asyncAssertSuccess());
-	}
+  /**
+   * Posts-conditions
+   * 
+   * @param context
+   */
+  @After
+  public void after(final TestContext context) {
+    vertx.close(context.asyncAssertSuccess());
+  }
 
-	/**
-	 * Test nominal : on vérifie l'adhérence du serveur avec les routes
-	 * configurées dans routes.properties.
-	 * 
-	 * @param context
-	 */
-	@Test
-	public void testHelloWorld(final TestContext context) {
-		final Async async = context.async();
+  /**
+   * Test nominal : on vérifie l'adhérence du serveur avec les routes configurées dans
+   * routes.properties.
+   * 
+   * @param context
+   */
+  @Test
+  public void testHelloWorld(final TestContext context) {
+    final Async async = context.async();
 
-		Integer port = propReader.getIntQuietly(APP_CONFIG_FILE, KEY_PORT);
-		String host = "localhost";
-		String requestURI = "/prodit-se/api/" + propReader.getQuietly(ROUTE_CONFIG_FILE, String.format(KEY_TPL_ROUTE_URL, "1"));
-		String expectedResponse = String.format(RESPONSE_HTML_TEMPLATE, "Hello world");
+    Integer port = propReader.getIntQuietly(APP_CONFIG_FILE, KEY_PORT);
+    String host = "localhost";
+    String requestURI = "/bblvertx/api/"
+        + propReader.getQuietly(ROUTE_CONFIG_FILE, String.format(KEY_TPL_ROUTE_URL, "1"));
+    String expectedResponse = String.format(RESPONSE_HTML_TEMPLATE, "Hello world");
 
-		vertx.createHttpClient().getNow(port, host, requestURI, response -> {
-			response.handler(body -> {
-				context.assertTrue(body.toString().contains(expectedResponse));
-				async.complete();
-			});
-		});
-	}
+    vertx.createHttpClient().getNow(port, host, requestURI, response -> {
+      response.handler(body -> {
+        context.assertTrue(body.toString().contains(expectedResponse));
+        async.complete();
+      });
+    });
+  }
 
-	/**
-	 * Test nominal : on vérifie l'adhérence avec Elastic Search.
-	 * 
-	 * @param context
-	 */
-	@Test
-	public void testSearchElasticSearch(final TestContext context) {
-		final Async async = context.async();
+  /**
+   * Test nominal : on vérifie l'adhérence avec Elastic Search.
+   * 
+   * @param context
+   */
+  @Test
+  public void testSearchElasticSearch(final TestContext context) {
+    final Async async = context.async();
 
-		Integer port = propReader.getIntQuietly(APP_CONFIG_FILE, KEY_PORT);
-		String host = "localhost";
-		String requestURI = "/prodit-se/api/" + propReader.getQuietly(ROUTE_CONFIG_FILE, String.format(KEY_TPL_ROUTE_URL, "3")) + "?startIndex=0&maxResults=100";
+    Integer port = propReader.getIntQuietly(APP_CONFIG_FILE, KEY_PORT);
+    String host = "localhost";
+    String requestURI = "/bblvertx/api/"
+        + propReader.getQuietly(ROUTE_CONFIG_FILE, String.format(KEY_TPL_ROUTE_URL, "3"))
+        + "?startIndex=0&maxResults=100";
 
-		vertx.createHttpClient().getNow(port, host, requestURI, response -> {
-			response.handler(body -> {
-				context.assertTrue(isNotEmpty(body.toString()));
-				async.complete();
-			});
-		});
-	}
+    vertx.createHttpClient().getNow(port, host, requestURI, response -> {
+      response.handler(body -> {
+        context.assertTrue(isNotEmpty(body.toString()));
+        async.complete();
+      });
+    });
+  }
 }
